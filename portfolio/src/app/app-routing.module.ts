b94a75a94ead5a19ecce74363c302eb7d6ad6959
kevin <kevin,andresfontt@gmail.com>
2023-07-18 15:07:44 -0400

    migracion a angular portafolio v2

--- portfolio/src/app/app-routing.module.ts
+++ portfolio/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProyectosComponent } from './proyectos/proyectos.component';
+import { ContactoComponent } from './contacto/contacto.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component:HomeComponent },
+  { path: 'proyectos', component: ProyectosComponent },
+  {path: 'contacto', component:ContactoComponent}
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
